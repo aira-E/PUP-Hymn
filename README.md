acf2a1a9e841004c5e3250e6a7b4f9c4a0fea7d1
Aira Estur <92619533+aira-E@users.noreply.github.com>
2023-04-09 02:28:50 +0800

    - initial commit

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ where the audio plays while the lyrics are scrolling.
 1. Button - The website has "play" button to control the play/pause of both audio and lyrics
 2. Responsive Design - The website used "@media" rule to make the design adapt to different screen sizes
 ___
-**Passed by: Aira Estur from BS in Computer Engineering 2-1** 
+** Passed by: Aira Estur from BS in Computer Engineering 2-1**
---
+++ script.js
@@ -0,0 +1,20 @@
+var audio = document.getElementById("audio");
+var playPauseBTN = document.getElementById("playPauseBTN");
+var hymnScroll = document.getElementById("hymn");
+var count = 0;
+
+function playPause()
+{
+  if(count == 0)
+  {
+   count = 1;
+   audio.play();  
+   hymnScroll.style.animationPlayState = "running";
+  }
+  else
+  {
+    count = 0;
+    audio.pause();
+    hymnScroll.style.animationPlayState = "paused";
+  }
+}
